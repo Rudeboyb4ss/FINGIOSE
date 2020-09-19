51298f2d2c06bf3ea3e5d209d96cfe8211d51d05
Florencia Corvalán <florencia.corvalan@usach.cl>
2020-09-19 20:41:51 -0300

    Se agrega backend

---
+++ pruebauno/src/main/java/yo/pruebauno/PruebaunoApplication.java
@@ -0,0 +1,13 @@
+package yo.pruebauno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaunoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaunoApplication.class, args);
+    }
+
+}
