a3f44813c311d962c5adcd9c17972e3fe704ca38
Jorge sandoval <jorge.sandoval.m@usach.cl>
2020-09-22 02:06:39 -0300

    Vista 4 completa y rutas para las vistas
    with love for FINGIOSE

--- gestorfilas/src/App.vue
+++ gestorfilas/src/App.vue
@@ -2,59 +2,28 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="#00C2CB"
       dark
+      max-height="100px"
+      width="100%"
     >
-      <div class="d-flex align-center">
+      <div class="flex align-center">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
+          alt="Filapp Logo"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="../public/Filapp.png"
           transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+          height="200px"
         />
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <!-- Esto llama a las rutas-->
+    <router-view/>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  name: 'App'
+}
 </script>
--- gestorfilas/src/views/Home.vue
+++ gestorfilas/src/views/Home.vue
@@ -1,18 +1,36 @@
+
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<v-container fluid ma-0 pa-0 fill-height class="cyan lighten-5">
+  <v-layout justify-center align-center>
+    <v-card
+    class="mx-auto"
+    width="344"
+    elevation="12"
+    height="400"
+    align="center"
+    justify="center"
+    >
+    <v-form 
+      class="mx-auto my-10"
+      ref="form"
+    >
+      <h1
+      class="mx-10"
+      >Ingresa código del local</h1>
+      <v-text-field
+        class="mx-10"
+        :counter="10"
+        label="Código"
+        required
+      ></v-text-field>
+      <v-btn
+        color="#00C2CB"
+        class="mr-4 mt-10 white--text"
+      >
+        Ingresar
+      </v-btn>
+    </v-form>
+    </v-card>
+  </v-layout>
+</v-container>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
