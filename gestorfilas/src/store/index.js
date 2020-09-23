839ecf54061adb2ed73c1762625fc7f1f705323a
Florencia Corvalán <florencia.corvalan@usach.cl>
2020-09-23 00:15:18 -0300

    Se conecta vista IngresoUsuario
    Se trabaja junto a Belén y Esteban.

--- gestorfilas/src/store/index.js
+++ gestorfilas/src/store/index.js
@@ -1,12 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    nuevo_user: {
+      rut: null,
+      nombre: null,
+      correo: null,
+    }
   },
   mutations: {
+    createUser(state, nuevo_user){
+      try{
+        console.log(nuevo_user);
+        axios.post('http://localhost:1818/user/create/', nuevo_user)
+        .then(response =>{
+          console.log(response);
+        }
+          )
+        .catch(function(error){
+          console.log(error);
+        });
+      } catch (err){
+        console.log("Hubo un problema al crear el usuario." + err)
+      }
+    }
+
+
   },
   actions: {
   },
